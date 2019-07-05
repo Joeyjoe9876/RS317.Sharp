277118bb05315bf1cfac4917928c489dff008e02
HelloKitty <andrew.blakely@ymail.com>
2019-07-05 02:53:49 -0500

    Fixed SoundFilter

--- src/Rs317.Library/audio/SoundFilter.cs
+++ src/Rs317.Library/audio/SoundFilter.cs
@@ -2,7 +2,6 @@
 //Some of this file was refactored by 'veer' of http://www.moparscape.org.
 
 using System;
-using System.Collections.ObjectModel;
 
 /// <summary>
 /// an impl of an adaptive iir filter that calculates
@@ -40,15 +39,15 @@ sealed class SoundFilter
 
 	private float adaptMagnitude(int direction, int i, float f)
 	{
-		float alpha = pairMagnitude[direction][0][i]
-				+ f * (pairMagnitude[direction][1][i] - pairMagnitude[direction][0][i]);
+		float alpha = pairMagnitude[direction, 0, i]
+				+ f * (pairMagnitude[direction, 1, i] - pairMagnitude[direction, 0, i]);
 		alpha *= 0.001525879F;
-		return 1.0F - (float)Math.pow(10D, -alpha / 20F);
+		return 1.0F - (float)Math.Pow(10D, -alpha / 20F);
 	}
 
 	private float adaptPhase(float f, int i, int direction)
 	{
-		float alpha = pairPhase[direction][0][i] + f * (pairPhase[direction][1][i] - pairPhase[direction][0][i]);
+		float alpha = pairPhase[direction, 0, i] + f * (pairPhase[direction, 1, i] - pairPhase[direction, 0, i]);
 		alpha *= 0.0001220703F;
 		return normalise(alpha);
 	}
@@ -60,38 +59,38 @@ sealed class SoundFilter
 		{
 			float f1 = unity[0] + (unity[1] - unity[0]) * f;
 			f1 *= 0.003051758F;
-			_invUnity = (float)Math.pow(0.10000000000000001D, f1 / 20F);
+			_invUnity = (float)Math.Pow(0.10000000000000001D, f1 / 20F);
 			invUnity = (int)(_invUnity * 65536F);
 		}
 		if(pairCount[direction] == 0)
 			return 0;
 		float _mag = adaptMagnitude(direction, 0, f);
-		_coefficient[direction][0] = -2F * _mag * (float)Math.cos(adaptPhase(f, 0, direction));
-		_coefficient[direction][1] = _mag * _mag;
+		_coefficient[direction, 0] = -2F * _mag * (float)Math.Cos(adaptPhase(f, 0, direction));
+		_coefficient[direction, 1] = _mag * _mag;
 		for(int pair = 1; pair < pairCount[direction]; pair++)
 		{
 			float mag = adaptMagnitude(direction, pair, f);
-			float phase = -2F * mag * (float)Math.cos(adaptPhase(f, pair, direction));
+			float phase = -2F * mag * (float)Math.Cos(adaptPhase(f, pair, direction));
 			float coeff = mag * mag;
-			_coefficient[direction][pair * 2 + 1] = _coefficient[direction][pair * 2 - 1] * coeff;
-			_coefficient[direction][pair * 2] = _coefficient[direction][pair * 2 - 1] * phase
-					+ _coefficient[direction][pair * 2 - 2] * coeff;
+			_coefficient[direction, pair * 2 + 1] = _coefficient[direction, pair * 2 - 1] * coeff;
+			_coefficient[direction, pair * 2] = _coefficient[direction, pair * 2 - 1] * phase
+					+ _coefficient[direction, pair * 2 - 2] * coeff;
 			for(int j1 = pair * 2 - 1; j1 >= 2; j1--)
-				_coefficient[direction][j1] += _coefficient[direction][j1 - 1] * phase
-						+ _coefficient[direction][j1 - 2] * coeff;
+				_coefficient[direction, j1] += _coefficient[direction, j1 - 1] * phase
+						+ _coefficient[direction, j1 - 2] * coeff;
 
-			_coefficient[direction][1] += _coefficient[direction][0] * phase + coeff;
-			_coefficient[direction][0] += phase;
+			_coefficient[direction, 1] += _coefficient[direction, 0] * phase + coeff;
+			_coefficient[direction, 0] += phase;
 		}
 
 		if(direction == 0)
 		{
 			for(int pair = 0; pair < pairCount[0] * 2; pair++)
-				_coefficient[0][pair] *= _invUnity;
+				_coefficient[0, pair] *= _invUnity;
 
 		}
 		for(int pair = 0; pair < pairCount[direction] * 2; pair++)
-			coefficient[direction][pair] = (int)(_coefficient[direction][pair] * 65536F);
+			coefficient[direction, pair] = (int)(_coefficient[direction, pair] * 65536F);
 
 		return pairCount[direction] * 2;
 	}
@@ -110,8 +109,8 @@ sealed class SoundFilter
 			{
 				for(int pair = 0; pair < pairCount[direction]; pair++)
 				{
-					pairPhase[direction][0][pair] = stream.getUnsignedLEShort();
-					pairMagnitude[direction][0][pair] = stream.getUnsignedLEShort();
+					pairPhase[direction, 0, pair] = stream.getUnsignedLEShort();
+					pairMagnitude[direction, 0, pair] = stream.getUnsignedLEShort();
 				}
 
 			}
@@ -121,13 +120,13 @@ sealed class SoundFilter
 				for(int pair = 0; pair < pairCount[direction]; pair++)
 					if((migrated & 1 << direction * 4 << pair) != 0)
 					{
-						pairPhase[direction][1][pair] = stream.getUnsignedLEShort();
-						pairMagnitude[direction][1][pair] = stream.getUnsignedLEShort();
+						pairPhase[direction, 1, pair] = stream.getUnsignedLEShort();
+						pairMagnitude[direction, 1, pair] = stream.getUnsignedLEShort();
 					}
 					else
 					{
-						pairPhase[direction][1][pair] = pairPhase[direction][0][pair];
-						pairMagnitude[direction][1][pair] = pairMagnitude[direction][0][pair];
+						pairPhase[direction, 1, pair] = pairPhase[direction, 0, pair];
+						pairMagnitude[direction, 1, pair] = pairMagnitude[direction, 0, pair];
 					}
 
 			}
@@ -143,7 +142,7 @@ sealed class SoundFilter
 
 	private float normalise(float alpha)
 	{
-		float f = 32.7032F * (float)Math.pow(2D, alpha);
+		float f = 32.7032F * (float)Math.Pow(2D, alpha);
 		return (f * 3.141593F) / 11025F;
 	}
 }
