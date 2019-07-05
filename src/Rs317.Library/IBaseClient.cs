952521db0dcbf8b36d84c67e7d0c75770bb67f5d
HelloKitty <andrew.blakely@ymail.com>
2019-07-05 14:49:06 -0500

    Added IBaseClient

---
+++ src/Rs317.Library/IBaseClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rs317
+{
+	public interface IBaseClient
+	{
+		FileCache[] caches { get; set; }
+
+		void startRunnable(IRunnable runnable, int priority);
+
+		Task<TcpClient> openSocket(int port);
+
+		bool isLoggedIn { get; }
+	}
+}
