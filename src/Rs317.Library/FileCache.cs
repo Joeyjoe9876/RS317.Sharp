c5a7138c4ce9852c92aefd085c0c4999e877fa2f
HelloKitty <andrew.blakely@ymail.com>
2019-07-05 09:22:21 -0500

    Fixed FileCache

--- src/Rs317.Library/FileCache.cs
+++ src/Rs317.Library/FileCache.cs
@@ -1,206 +1,213 @@
 
+using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+
 /// <summary>
 /// Represents a file cache containing multiple archives.
 /// </summary>
 sealed class FileCache
 {
 	private static byte[] buffer = new byte[520];
-	private RandomAccessFile dataFile;
-	private RandomAccessFile indexFile;
+	private FileStream dataFile;
+	private FileStream indexFile;
 	private int storeId;
 
-	public FileCache(RandomAccessFile data, RandomAccessFile index, int storeId)
+	public FileCache(FileStream data, FileStream index, int storeId)
 	{
 		this.storeId = storeId;
 		this.dataFile = data;
 		this.indexFile = index;
 	}
 
-	public synchronized byte[] decompress(int index)
+	[MethodImpl(MethodImplOptions.Synchronized)]
+	public byte[] decompress(int index)
 	{
 		try
 		{
 			seek(indexFile, index * 6);
-			int in;
-			for(int r = 0; r < 6; r += in)
+			int inValue;
+			for(int r = 0; r < 6; r += inValue)
 			{
-				in = indexFile.read(buffer, r, 6 - r);
-				if(in == -1)
-					return null;
-		}
-
-			int size = ((buffer[0] & 0xff) << 16) + ((buffer[1] & 0xff) << 8) + (buffer[2] & 0xff);
-		int sector = ((buffer[3] & 0xff) << 16) + ((buffer[4] & 0xff) << 8) + (buffer[5] & 0xff);
-
-		if(size < 0 || size > 0x7a120)
-			return null;
-
-		if(sector <= 0 || sector > dataFile.length() / 520L)
-			return null;
-
-		byte decompressed[] = new byte[size];
-		int read = 0;
-		for(int part = 0; read < size; part++)
-		{
-			if(sector == 0)
-				return null;
-			seek(dataFile, sector * 520);
-			int r = 0;
-			int unread = size - read;
-			if(unread > 512)
-				unread = 512;
-			int in_;
-			for(; r < unread + 8; r += in_)
-			{
-				in_ = dataFile.read(buffer, r, (unread + 8) - r);
-				if(in_ == -1)
+				inValue = indexFile.Read(buffer, r, 6 - r);
+				if(inValue == -1)
 					return null;
 			}
 
-			int decompressedIndex = ((buffer[0] & 0xff) << 8) + (buffer[1] & 0xff);
-			int decompressedPart = ((buffer[2] & 0xff) << 8) + (buffer[3] & 0xff);
-			int decompressedSector = ((buffer[4] & 0xff) << 16) + ((buffer[5] & 0xff) << 8) + (buffer[6] & 0xff);
-			int decompressedStoreId = buffer[7] & 0xff;
+			int size = ((buffer[0] & 0xff) << 16) + ((buffer[1] & 0xff) << 8) + (buffer[2] & 0xff);
+			int sector = ((buffer[3] & 0xff) << 16) + ((buffer[4] & 0xff) << 8) + (buffer[5] & 0xff);
 
-			if(decompressedIndex != index || decompressedPart != part || decompressedStoreId != storeId)
+			if(size < 0 || size > 0x7a120)
 				return null;
 
-			if(decompressedSector < 0 || decompressedSector > dataFile.length() / 520L)
+			if(sector <= 0 || sector > dataFile.Length / 520L)
 				return null;
 
-			for(int i = 0; i < unread; i++)
-				decompressed[read++] = buffer[i + 8];
+			byte[] decompressed = new byte[size];
+			int read = 0;
+			for(int part = 0; read < size; part++)
+			{
+				if(sector == 0)
+					return null;
+				seek(dataFile, sector * 520);
+				int r = 0;
+				int unread = size - read;
+				if(unread > 512)
+					unread = 512;
+				int in_;
+				for(; r < unread + 8; r += in_)
+				{
+					in_ = dataFile.Read(buffer, r, (unread + 8) - r);
+					if(in_ == -1)
+						return null;
+				}
 
-			sector = decompressedSector;
+				int decompressedIndex = ((buffer[0] & 0xff) << 8) + (buffer[1] & 0xff);
+				int decompressedPart = ((buffer[2] & 0xff) << 8) + (buffer[3] & 0xff);
+				int decompressedSector = ((buffer[4] & 0xff) << 16) + ((buffer[5] & 0xff) << 8) + (buffer[6] & 0xff);
+				int decompressedStoreId = buffer[7] & 0xff;
+
+				if(decompressedIndex != index || decompressedPart != part || decompressedStoreId != storeId)
+					return null;
+
+				if(decompressedSector < 0 || decompressedSector > dataFile.Length / 520L)
+					return null;
+
+				for(int i = 0; i < unread; i++)
+					decompressed[read++] = buffer[i + 8];
+
+				sector = decompressedSector;
+			}
+
+			return decompressed;
 		}
-
-		return decompressed;
-	} catch (IOException _ex) {
+		catch(IOException _ex)
+		{
 			return null;
 		}
 	}
 
-	public synchronized bool put(int size, byte data[], int index)
-{
-	bool exists = put(true, index, size, data);
-	if(!exists)
-		exists = put(false, index, size, data);
-	return exists;
-}
-
-private synchronized bool put(bool exists, int index, int size, byte data[])
-{
-	try
+	[MethodImpl(MethodImplOptions.Synchronized)]
+	public bool put(int size, byte[] data, int index)
 	{
-		int sector;
-		if(exists)
-		{
-			seek(indexFile, index * 6);
-			int in;
-			for(int r = 0; r < 6; r += in)
-			{
-					in = indexFile.read(buffer, r, 6 - r);
-				if(in == -1)
-						return false;
-		}
-
-		sector = ((buffer[3] & 0xff) << 16) + ((buffer[4] & 0xff) << 8) + (buffer[5] & 0xff);
-		if(sector <= 0 || sector > dataFile.length() / 520L)
-			return false;
-	} else
-	{
-		sector = (int)((dataFile.length() + 519L) / 520L);
-		if(sector == 0)
-			sector = 1;
+		bool exists = put(true, index, size, data);
+		if(!exists)
+			exists = put(false, index, size, data);
+		return exists;
 	}
-	buffer[0] = (byte)(size >> 16);
-	buffer[1] = (byte)(size >> 8);
-	buffer[2] = (byte)size;
-	buffer[3] = (byte)(sector >> 16);
-	buffer[4] = (byte)(sector >> 8);
-	buffer[5] = (byte)sector;
-	seek(indexFile, index * 6);
-	indexFile.write(buffer, 0, 6);
-	int written = 0;
-	for(int part = 0; written < size; part++)
-	{
-		int decompressedSector = 0;
-		if(exists)
-		{
-			seek(dataFile, sector * 520);
-			int read;
-			int in;
-			for(read = 0; read < 8; read += in)
-			{
-						in = dataFile.read(buffer, read, 8 - read);
-				if(in == -1)
-							break;
-}
 
-					if (read == 8) {
+	[MethodImpl(MethodImplOptions.Synchronized)]
+	private bool put(bool exists, int index, int size, byte[] data)
+	{
+		try
+		{
+			int sector;
+			if(exists)
+			{
+				seek(indexFile, index * 6);
+				int inValue;
+				for(int r = 0; r < 6; r += inValue)
+				{
+					inValue = indexFile.Read(buffer, r, 6 - r);
+					if(inValue == -1)
+						return false;
+				}
+
+				sector = ((buffer[3] & 0xff) << 16) + ((buffer[4] & 0xff) << 8) + (buffer[5] & 0xff);
+				if(sector <= 0 || sector > dataFile.Length / 520L)
+					return false;
+			}
+			else
+			{
+				sector = (int)((dataFile.Length + 519L) / 520L);
+				if(sector == 0)
+					sector = 1;
+			}
+			buffer[0] = (byte)(size >> 16);
+			buffer[1] = (byte)(size >> 8);
+			buffer[2] = (byte)size;
+			buffer[3] = (byte)(sector >> 16);
+			buffer[4] = (byte)(sector >> 8);
+			buffer[5] = (byte)sector;
+			seek(indexFile, index * 6);
+			indexFile.Write(buffer, 0, 6);
+			int written = 0;
+			for(int part = 0; written < size; part++)
+			{
+				int decompressedSector = 0;
+				if(exists)
+				{
+					seek(dataFile, sector * 520);
+					int read;
+					int inValue;
+					for(read = 0; read < 8; read += inValue)
+					{
+						inValue = dataFile.Read(buffer, read, 8 - read);
+						if(inValue == -1)
+							break;
+					}
+
+					if(read == 8)
+					{
 						int decompressedIndex = ((buffer[0] & 0xff) << 8) + (buffer[1] & 0xff);
-int decompressedPart = ((buffer[2] & 0xff) << 8) + (buffer[3] & 0xff);
-decompressedSector = ((buffer[4] & 0xff) << 16) + ((buffer[5] & 0xff) << 8)
-								+ (buffer[6] & 0xff);
+						int decompressedPart = ((buffer[2] & 0xff) << 8) + (buffer[3] & 0xff);
+						decompressedSector = ((buffer[4] & 0xff) << 16) + ((buffer[5] & 0xff) << 8)
+													+ (buffer[6] & 0xff);
 						int decompressedStoreId = buffer[7] & 0xff;
-						if (decompressedIndex != index || decompressedPart != part || decompressedStoreId != storeId)
+						if(decompressedIndex != index || decompressedPart != part || decompressedStoreId != storeId)
 							return false;
-						if (decompressedSector< 0 || decompressedSector> dataFile.length() / 520L)
+						if(decompressedSector < 0 || decompressedSector > dataFile.Length / 520L)
 							return false;
 					}
 				}
 
-				if (decompressedSector == 0) {
+				if(decompressedSector == 0)
+				{
 					exists = false;
-					decompressedSector = (int) ((dataFile.length() + 519L) / 520L);
-					if (decompressedSector == 0)
+					decompressedSector = (int)((dataFile.Length + 519L) / 520L);
+					if(decompressedSector == 0)
 						decompressedSector++;
-					if (decompressedSector == sector)
+					if(decompressedSector == sector)
 						decompressedSector++;
 				}
 
-				if (size - written <= 512)
+				if(size - written <= 512)
 					decompressedSector = 0;
 
-				buffer[0] = (byte) (index >> 8);
-				buffer[1] = (byte) index;
-buffer[2] = (byte) (part >> 8);
-				buffer[3] = (byte) part;
-buffer[4] = (byte) (decompressedSector >> 16);
-				buffer[5] = (byte) (decompressedSector >> 8);
-				buffer[6] = (byte) decompressedSector;
-buffer[7] = (byte) storeId;
-seek(dataFile, sector* 520);
-dataFile.write(buffer, 0, 8);
+				buffer[0] = (byte)(index >> 8);
+				buffer[1] = (byte)index;
+				buffer[2] = (byte)(part >> 8);
+				buffer[3] = (byte)part;
+				buffer[4] = (byte)(decompressedSector >> 16);
+				buffer[5] = (byte)(decompressedSector >> 8);
+				buffer[6] = (byte)decompressedSector;
+				buffer[7] = (byte)storeId;
+				seek(dataFile, sector * 520);
+				dataFile.Write(buffer, 0, 8);
 
 				int unwritten = size - written;
-				if (unwritten > 512)
+				if(unwritten > 512)
 					unwritten = 512;
-				dataFile.write(data, written, unwritten);
+				dataFile.Write(data, written, unwritten);
 				written += unwritten;
 				sector = decompressedSector;
 			}
 			return true;
-		} catch (IOException _ex) {
+		}
+		catch(IOException _ex)
+		{
 			return false;
 		}
 	}
 
-
-	private synchronized void seek(RandomAccessFile file, int position)
-{
-	if(position < 0 || position > 0x3c00000)
+	[MethodImpl(MethodImplOptions.Synchronized)]
+	private void seek(FileStream file, int position)
 	{
-		System.out.println("Badseek - pos:" + position + " len:" + file.length());
-		position = 0x3c00000;
-		try
-		{
-			Thread.sleep(1000L);
-		}
-		catch(Exception _ex)
-		{
-		}
+		if (file == null) throw new ArgumentNullException(nameof(file));
+
+		if(position < 0 || position > 0x3c00000)
+			throw new InvalidOperationException($"Badseek - pos:{position} len:{file.Length}");
+
+		file.Seek(position, SeekOrigin.Begin);
 	}
-	file.seek(position);
-}
 }
