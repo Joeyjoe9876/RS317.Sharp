785f51ca4fbc7dfabd165e9eac565f87c1cda89e
HelloKitty <andrew.blakely@ymail.com>
2019-07-05 08:03:11 -0500

    Added IRunnable interface

---
+++ src/Rs317.Library/IRunnable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+//TODO: Create namespace.
+/// <summary>
+/// Based on the Java runnable.
+/// </summary>
+public interface IRunnable
+{
+	//Lowecase to match java.
+	/// <summary>
+	/// The run method of the runnable.
+	/// </summary>
+	void run();
+}
