5dd26d764bf59eee3b5772ac64b43489f17043cd
HelloKitty <andrew.blakely@ymail.com>
2019-07-06 01:51:20 -0500

    Added hacky fix for missing BZip2 header

--- src/Rs317.Library/bzip2/Bzip2Decompressor.cs
+++ src/Rs317.Library/bzip2/Bzip2Decompressor.cs
@@ -10,12 +10,20 @@ using ICSharpCode.SharpZipLib.BZip2;
 /// </summary>
 public sealed class Bzip2Decompressor
 {
+	//Jagex uses headerless BZip2 because they're INSANE like other game dev studios.
+	public static byte[] BZIP2_HEADER = { 66, 90, 104, 49 };
+
 	//Renamed retVal to inputLength
 	public static int decompress(byte[] output, int expectedOutputSize, byte[] input, int maxLen, int inputOffset)
 	{
-		using (MemoryStream inputStream = new MemoryStream(input, inputOffset, input.Length - inputOffset, false))
+		//Have to include BZIP2 Header
+		//using (MemoryStream inputStream = new MemoryStream(input, inputOffset, input.Length - inputOffset, false))
+		using (MemoryStream inputStream = new MemoryStream(input.Length - inputOffset + 4))
 		{
-			using (MemoryStream outputStream = new MemoryStream(output, 0, maxLen, true))
+			inputStream.Write(BZIP2_HEADER, 0, 4);
+			inputStream.Write(input, inputOffset, input.Length - inputOffset);
+			inputStream.Position = 0;
+			using (MemoryStream outputStream = new MemoryStream(output, 0, expectedOutputSize, true))
 			{
 				BZip2.Decompress(inputStream, outputStream, true);
 			}
