7d64895d82ddf78a1e23bd103235f9c1f170408a
HelloKitty <andrew.blakely@ymail.com>
2019-07-05 23:05:06 -0500

    Added IRunnableStarter

---
+++ src/Rs317.Library/Custom/IRunnableStarter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317
+{
+	public interface IRunnableStarter
+	{
+		void StartRunnable(IRunnable runnable, int priority);
+	}
+}
