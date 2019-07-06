d07b5b16148915858491ef114c2e032af1de9804
HelloKitty <andrew.blakely@ymail.com>
2019-07-06 13:00:04 -0500

    Fixed bitshiftting in IsaacRandomGenerator
    I didn't fully grasp the consequences of the unsigned shifting in Java

--- src/Rs317.Library/isaac/ISAACRandomGenerator.cs
+++ src/Rs317.Library/isaac/ISAACRandomGenerator.cs
@@ -1,50 +1,61 @@
 
+
+public static class ISAACRandomGeneratorExtensions
+{
+	//Extension method to support switching between RS2Sharp implementations and the renamed 317refactor.
+	public static int getNextKey(this ISAACRandomGenerator generator)
+	{
+		return generator.value();
+	}
+}
+
 /// <summary>
 /// An implementation of the ISAAC cipher
-/// 
+///
 /// Based on Rand.java by Bob Jenkins
+///
 /// http://burtleburtle.net/bob/java/rand/Rand.java
 /// </summary>
-public sealed class ISAACRandomGenerator
+public class ISAACRandomGenerator
 {
+
 	/**
 	 * log of size of results[] and memory[]
 	 */
-	static int SIZEL = 8;
+	private readonly static int SIZEL = 8;
 
 	/**
 	 * size of results[] and memory[]
 	 */
-	static int SIZE = 1 << SIZEL;
+	private readonly static int SIZE = 1 << SIZEL;
 
 	/**
 	 * for pseudorandom lookup
 	 */
-	static int MASK = (SIZE - 1) << 2;
+	private readonly static int MASK = (SIZE - 1) << 2;
 
 	private int count;
-	private int[] results;
-	private int[] memory;
+	private readonly int[] results;
+	private readonly int[] memory;
 	private int accumulator;
 	private int lastResult;
 	private int counter;
 
-	/**
-	 * Create a new ISAAC generator with a given seed.
-	 * 
-	 * @param seed The seed.
-	 */
+	/// <summary>
+	/// Create a new ISAAC generator with a given seed.
+	/// </summary>
+	/// <param name="seed">The generation seed.</param>
 	public ISAACRandomGenerator(int[] seed)
 	{
-		memory = new int[SIZE];
-		results = new int[SIZE];
-		System.Buffer.BlockCopy(seed, 0, results, 0, seed.Length * sizeof(int));
-		initialise();
+		this.memory = new int[SIZE];
+		this.results = new int[SIZE];
+		System.Buffer.BlockCopy(seed, 0, this.results, 0, sizeof(int) * seed.Length);
+		this.initialise();
 	}
 
-	/**
-	 * Initialise or reinitialise the generator.
-	 */
+	/// <summary>
+	/// Initialise or reinitialise the generator.
+	/// </summary>
 	private void initialise()
 	{
 		int a, b, c, d, e, f, g, h;
@@ -54,181 +65,181 @@ public sealed class ISAACRandomGenerator
 			a ^= b << 11;
 			d += a;
 			b += c;
-			b ^= c >> 2;
+			b ^= (int)((uint)c >> 2);
 			e += b;
 			c += d;
 			c ^= d << 8;
 			f += c;
 			d += e;
-			d ^= e >> 16;
+			d ^= (int)((uint)e >> 16);
 			g += d;
 			e += f;
 			e ^= f << 10;
 			h += e;
 			f += g;
-			f ^= g >> 4;
+			f ^= (int)((uint)g >> 4);
 			a += f;
 			g += h;
 			g ^= h << 8;
 			b += g;
 			h += a;
-			h ^= a >> 9;
+			h ^= (int)((uint)a >> 9);
 			c += h;
 			a += b;
 		}
 
 		for(int i = 0; i < SIZE; i += 8)
 		{
-			a += results[i];
-			b += results[i + 1];
-			c += results[i + 2];
-			d += results[i + 3];
-			e += results[i + 4];
-			f += results[i + 5];
-			g += results[i + 6];
-			h += results[i + 7];
+			a += this.results[i];
+			b += this.results[i + 1];
+			c += this.results[i + 2];
+			d += this.results[i + 3];
+			e += this.results[i + 4];
+			f += this.results[i + 5];
+			g += this.results[i + 6];
+			h += this.results[i + 7];
 			a ^= b << 11;
 			d += a;
 			b += c;
-			b ^= c >> 2;
+			b ^= (int)((uint)c >> 2);
 			e += b;
 			c += d;
 			c ^= d << 8;
 			f += c;
 			d += e;
-			d ^= e >> 16;
+			d ^= (int)((uint)e >> 16);
 			g += d;
 			e += f;
 			e ^= f << 10;
 			h += e;
 			f += g;
-			f ^= g >> 4;
+			f ^= (int)((uint)g >> 4);
 			a += f;
 			g += h;
 			g ^= h << 8;
 			b += g;
 			h += a;
-			h ^= a >> 9;
+			h ^= (int)((uint)a >> 9);
 			c += h;
 			a += b;
-			memory[i] = a;
-			memory[i + 1] = b;
-			memory[i + 2] = c;
-			memory[i + 3] = d;
-			memory[i + 4] = e;
-			memory[i + 5] = f;
-			memory[i + 6] = g;
-			memory[i + 7] = h;
+			this.memory[i] = a;
+			this.memory[i + 1] = b;
+			this.memory[i + 2] = c;
+			this.memory[i + 3] = d;
+			this.memory[i + 4] = e;
+			this.memory[i + 5] = f;
+			this.memory[i + 6] = g;
+			this.memory[i + 7] = h;
 		}
 
 		for(int i = 0; i < SIZE; i += 8)
 		{
-			a += memory[i];
-			b += memory[i + 1];
-			c += memory[i + 2];
-			d += memory[i + 3];
-			e += memory[i + 4];
-			f += memory[i + 5];
-			g += memory[i + 6];
-			h += memory[i + 7];
+			a += this.memory[i];
+			b += this.memory[i + 1];
+			c += this.memory[i + 2];
+			d += this.memory[i + 3];
+			e += this.memory[i + 4];
+			f += this.memory[i + 5];
+			g += this.memory[i + 6];
+			h += this.memory[i + 7];
 			a ^= b << 11;
 			d += a;
 			b += c;
-			b ^= c >> 2;
+			b ^= (int)((uint)c >> 2);
 			e += b;
 			c += d;
 			c ^= d << 8;
 			f += c;
 			d += e;
-			d ^= e >> 16;
+			d ^= (int)((uint)e >> 16);
 			g += d;
 			e += f;
 			e ^= f << 10;
 			h += e;
 			f += g;
-			f ^= g >> 4;
+			f ^= (int)((uint)g >> 4);
 			a += f;
 			g += h;
 			g ^= h << 8;
 			b += g;
 			h += a;
-			h ^= a >> 9;
+			h ^= (int)((uint)a >> 9);
 			c += h;
 			a += b;
-			memory[i] = a;
-			memory[i + 1] = b;
-			memory[i + 2] = c;
-			memory[i + 3] = d;
-			memory[i + 4] = e;
-			memory[i + 5] = f;
-			memory[i + 6] = g;
-			memory[i + 7] = h;
+			this.memory[i] = a;
+			this.memory[i + 1] = b;
+			this.memory[i + 2] = c;
+			this.memory[i + 3] = d;
+			this.memory[i + 4] = e;
+			this.memory[i + 5] = f;
+			this.memory[i + 6] = g;
+			this.memory[i + 7] = h;
 		}
 
-		isaac();
-		count = SIZE;
+		this.isaac();
+		this.count = SIZE;
 	}
 
 	/**
 	 * Generate 256 random results.
 	 */
-	public void isaac()
+	private void isaac()
 	{
 		int a, b, x, y;
 
-		lastResult += ++counter;
+		this.lastResult += ++this.counter;
 		for(a = 0, b = SIZE / 2; a < SIZE / 2;)
 		{
-			x = memory[a];
-			accumulator ^= accumulator << 13;
-			accumulator += memory[b++];
-			memory[a] = y = memory[(x & MASK) >> 2] + accumulator + lastResult;
-			results[a++] = lastResult = memory[((y >> SIZEL) & MASK) >> 2] + x;
+			x = this.memory[a];
+			this.accumulator ^= this.accumulator << 13;
+			this.accumulator += this.memory[b++];
+			this.memory[a] = y = this.memory[(x & MASK) >> 2] + this.accumulator + this.lastResult;
+			this.results[a++] = this.lastResult = this.memory[((y >> SIZEL) & MASK) >> 2] + x;
 
-			x = memory[a];
-			accumulator ^= accumulator >> 6;
-			accumulator += memory[b++];
-			memory[a] = y = memory[(x & MASK) >> 2] + accumulator + lastResult;
-			results[a++] = lastResult = memory[((y >> SIZEL) & MASK) >> 2] + x;
+			x = this.memory[a];
+			this.accumulator ^= (int)((uint)this.accumulator >> 6);
+			this.accumulator += this.memory[b++];
+			this.memory[a] = y = this.memory[(x & MASK) >> 2] + this.accumulator + this.lastResult;
+			this.results[a++] = this.lastResult = this.memory[((y >> SIZEL) & MASK) >> 2] + x;
 
-			x = memory[a];
-			accumulator ^= accumulator << 2;
-			accumulator += memory[b++];
-			memory[a] = y = memory[(x & MASK) >> 2] + accumulator + lastResult;
-			results[a++] = lastResult = memory[((y >> SIZEL) & MASK) >> 2] + x;
+			x = this.memory[a];
+			this.accumulator ^= this.accumulator << 2;
+			this.accumulator += this.memory[b++];
+			this.memory[a] = y = this.memory[(x & MASK) >> 2] + this.accumulator + this.lastResult;
+			this.results[a++] = this.lastResult = this.memory[((y >> SIZEL) & MASK) >> 2] + x;
 
-			x = memory[a];
-			accumulator ^= accumulator >> 16;
-			accumulator += memory[b++];
-			memory[a] = y = memory[(x & MASK) >> 2] + accumulator + lastResult;
-			results[a++] = lastResult = memory[((y >> SIZEL) & MASK) >> 2] + x;
+			x = this.memory[a];
+			this.accumulator ^= (int)((uint)this.accumulator >> 16);
+			this.accumulator += this.memory[b++];
+			this.memory[a] = y = this.memory[(x & MASK) >> 2] + this.accumulator + this.lastResult;
+			this.results[a++] = this.lastResult = this.memory[((y >> SIZEL) & MASK) >> 2] + x;
 		}
 
 		for(b = 0; b < SIZE / 2;)
 		{
-			x = memory[a];
-			accumulator ^= accumulator << 13;
-			accumulator += memory[b++];
-			memory[a] = y = memory[(x & MASK) >> 2] + accumulator + lastResult;
-			results[a++] = lastResult = memory[((y >> SIZEL) & MASK) >> 2] + x;
+			x = this.memory[a];
+			this.accumulator ^= this.accumulator << 13;
+			this.accumulator += this.memory[b++];
+			this.memory[a] = y = this.memory[(x & MASK) >> 2] + this.accumulator + this.lastResult;
+			this.results[a++] = this.lastResult = this.memory[((y >> SIZEL) & MASK) >> 2] + x;
 
-			x = memory[a];
-			accumulator ^= accumulator >> 6;
-			accumulator += memory[b++];
-			memory[a] = y = memory[(x & MASK) >> 2] + accumulator + lastResult;
-			results[a++] = lastResult = memory[((y >> SIZEL) & MASK) >> 2] + x;
+			x = this.memory[a];
+			this.accumulator ^= (int)((uint)this.accumulator >> 6);
+			this.accumulator += this.memory[b++];
+			this.memory[a] = y = this.memory[(x & MASK) >> 2] + this.accumulator + this.lastResult;
+			this.results[a++] = this.lastResult = this.memory[((y >> SIZEL) & MASK) >> 2] + x;
 
-			x = memory[a];
-			accumulator ^= accumulator << 2;
-			accumulator += memory[b++];
-			memory[a] = y = memory[(x & MASK) >> 2] + accumulator + lastResult;
-			results[a++] = lastResult = memory[((y >> SIZEL) & MASK) >> 2] + x;
+			x = this.memory[a];
+			this.accumulator ^= this.accumulator << 2;
+			this.accumulator += this.memory[b++];
+			this.memory[a] = y = this.memory[(x & MASK) >> 2] + this.accumulator + this.lastResult;
+			this.results[a++] = this.lastResult = this.memory[((y >> SIZEL) & MASK) >> 2] + x;
 
-			x = memory[a];
-			accumulator ^= accumulator >> 16;
-			accumulator += memory[b++];
-			memory[a] = y = memory[(x & MASK) >> 2] + accumulator + lastResult;
-			results[a++] = lastResult = memory[((y >> SIZEL) & MASK) >> 2] + x;
+			x = this.memory[a];
+			this.accumulator ^= (int)((uint)this.accumulator >> 16);
+			this.accumulator += this.memory[b++];
+			this.memory[a] = y = this.memory[(x & MASK) >> 2] + this.accumulator + this.lastResult;
+			this.results[a++] = this.lastResult = this.memory[((y >> SIZEL) & MASK) >> 2] + x;
 		}
 	}
 
@@ -239,11 +250,11 @@ public sealed class ISAACRandomGenerator
 	 */
 	public int value()
 	{
-		if(count-- == 0)
+		if(this.count-- == 0)
 		{
-			isaac();
-			count = (SIZE - 1);
+			this.isaac();
+			this.count = (SIZE - 1);
 		}
-		return results[count];
+		return this.results[this.count];
 	}
 }
