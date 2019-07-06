9319dcd8616e71fea8a0cb17e3ddd77be38640d3
HelloKitty <andrew.blakely@ymail.com>
2019-07-05 23:15:16 -0500

    Added IMouseInputQueryable

---
+++ src/Rs317.Library/Custom/IMouseInputQueryable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317
+{
+	//Basically, this exists to seperate the MouseDetection class from a dependency on the Client Type.
+	/// <summary>
+	/// Conctract for a type that exposes mouse information/data.
+	/// </summary>
+	public interface IMouseInputQueryable
+	{
+		//Lowercast to match Java
+		int mouseX { get; }
+
+		int mouseY { get; }
+	}
+}
