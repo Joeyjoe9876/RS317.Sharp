11eb27061a9958112e4355a07b43e02e33f34f07
HelloKitty <andrew.blakely@ymail.com>
2019-07-09 16:32:45 -0500

    Added ServerSetClientWalkableInterfacePayload

---
+++ src/Rs317.Extended.Packets/Packets/Interface/ServerSetClientWalkableInterfacePayload.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace Rs317.Extended
+{
+	[WireDataContract]
+	[GameClientPayload(RsNetworkOperationCode.SetClientWalkableInterface)]
+	public sealed class ServerSetClientWalkableInterfacePayload : BaseGameServerPayload
+	{
+		/// <summary>
+		/// The interface ID to set.
+		/// </summary>
+		[WireMember(1)]
+		public short InterfaceId { get; private set; }
+
+		public ServerSetClientWalkableInterfacePayload(short interfaceId)
+		{
+			InterfaceId = interfaceId;
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private ServerSetClientWalkableInterfacePayload()
+		{
+			
+		}
+	}
+}
