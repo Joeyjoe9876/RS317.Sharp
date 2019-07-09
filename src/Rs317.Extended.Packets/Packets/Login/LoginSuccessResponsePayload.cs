a45e485666c0adddcdb829e8c18355cf99efe327
HelloKitty <andrew.blakely@ymail.com>
2019-07-09 15:02:29 -0500

    Added LoginSuccessResponsePayload

---
+++ src/Rs317.Extended.Packets/Packets/Login/LoginSuccessResponsePayload.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace Rs317.Extended
+{
+	[WireDataContract]
+	[GameClientPayload(RsNetworkOperationCode.LoginSuccessful)]
+	public sealed class LoginSuccessResponsePayload : BaseGameClientPayload
+	{
+		/// <summary>
+		/// AKA the serverSessionKey.
+		/// Used to initialize the packet header encryption.
+		/// </summary>
+		[WireMember(1)]
+		public long ServerEncryptionInitializationVector { get; private set; }
+
+		public LoginSuccessResponsePayload(long serverEncryptionInitializationVector)
+		{
+			ServerEncryptionInitializationVector = serverEncryptionInitializationVector;
+		}
+
+		private LoginSuccessResponsePayload()
+		{
+			
+		}
+	}
+}
---
+++ src/Rs317.Extended.Packets/RsNetworkOperationCode.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Extended
+{
+	/// <summary>
+	/// Operation codes for the RS317 packets.
+	/// </summary>
+	public enum RsNetworkOperationCode : byte
+	{
+		/// <summary>
+		/// Operation code for packet type that indicates a login
+		/// success.
+		/// </summary>
+		LoginSuccessful = 2,
+	}
+}
