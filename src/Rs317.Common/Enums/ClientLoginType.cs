ab593f02e927284868de5691199251ccacf28896
HelloKitty <andrew.blakely@ymail.com>
2019-07-09 15:56:54 -0500

    Added ClientLoginType

---
+++ src/Rs317.Common/Enums/ClientLoginType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public enum ClientLoginType : byte
+	{
+		CreateSession = 16,
+
+		ClaimExistingSession = 18,
+	}
+}
