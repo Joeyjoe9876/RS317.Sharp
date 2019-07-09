5dd9d18fad817e172bc53daf5cfc8977abf6d7ac
HelloKitty <andrew.blakely@ymail.com>
2019-07-09 14:59:05 -0500

    Added BaseGameClientPayload

---
+++ src/Rs317.Extended.Packets/Packets/BaseGameClientPayload.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace Rs317.Extended
+{
+	[WireDataContract]
+	public abstract class BaseGameClientPayload
+	{
+		
+	}
+}
