cfa8bd10ad38f710d6d1b774c955dafac0bf0363
HelloKitty <andrew.blakely@ymail.com>
2019-07-09 17:08:02 -0500

    Added custom byte type

---
+++ src/Rs317.Extended.Packets/Primitives/CustomByteA.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace Rs317.Extended
+{
+	[WireDataContract]
+	public class CustomByteA : CustomByteType
+	{
+		public CustomByteA(byte actualValue) 
+			: base(actualValue)
+		{
+
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private CustomByteA()
+		{
+			
+		}
+
+		protected override byte ComputeCustomValue()
+		{
+			return (byte) (ActualValue - 128 & 0xff);
+		}
+	}
+}
---
+++ src/Rs317.Extended.Packets/Primitives/CustomByteC.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore.Serializer;
+
+namespace Rs317.Extended
+{
+	[WireDataContract]
+	public class CustomByteC : CustomByteType
+	{
+		public CustomByteC(byte actualValue) 
+			: base(actualValue)
+		{
+
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private CustomByteC()
+		{
+			
+		}
+
+		protected override byte ComputeCustomValue()
+		{
+			return (byte) (-ActualValue & 0xff);
+		}
+	}
+}
