9c9b54d7c3b64679e479e17fdbe57644faa490e7
HelloKitty <andrew.blakely@ymail.com>
2019-07-10 16:03:49 -0500

    Fixed OpenTK.Extended startup

--- src/Client/Rs317.Client.OpenTK.Extended/Program.cs
+++ src/Client/Rs317.Client.OpenTK.Extended/Program.cs
@@ -6,9 +6,24 @@ namespace Rs317.Extended
 {
 	public class ExtendedProgram : Program
 	{
-		public new static Task Main(string[] args)
+		public new static async Task Main(string[] args)
 		{
-			return Program.Main(args);
+			Console.WriteLine($"RS2 user client - release #{317} using Rs317.Sharp.Extended by Glader");
+
+			try
+			{
+				ExtendedProgram program = new ExtendedProgram();
+				await program.StartClient(0, 0, true);
+			}
+			catch(Exception exception)
+			{
+				throw new InvalidOperationException($"Erorr: {exception.Message} \n\n Stack: {exception.StackTrace}");
+			}
+		}
+
+		public override OpenTKClient CreateOpenTkClient(ClientConfiguration configuration, OpenTkImageProducerFactory imageProducerFactory)
+		{
+			return base.CreateOpenTkClient(configuration, imageProducerFactory);
 		}
 	}
 }
