a31c83a1e9885dc1eadab95da13bbf5ae6cce3ac
HelloKitty <andrew.blakely@ymail.com>
2019-07-10 12:59:02 -0500

    Added IBufferFactory

---
+++ src/Rs317.Library/Custom/Factory/EmptyFactoryCreationContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public sealed class EmptyFactoryCreationContext
+	{
+		public static EmptyFactoryCreationContext Instance { get; } = new EmptyFactoryCreationContext();
+
+		private EmptyFactoryCreationContext()
+		{
+			
+		}
+	}
+}
