cb8d3f84f321194ba2a5b11597458ce4f6b3bdc2
HelloKitty <andrew.blakely@ymail.com>
2019-07-10 13:50:22 -0500

    Simplified DefaultBufferFactory

--- src/Rs317.Library/Custom/Factory/Buffer/DefaultBufferFactory.cs
+++ src/Rs317.Library/Custom/Factory/Buffer/DefaultBufferFactory.cs
@@ -8,11 +8,8 @@ namespace Rs317.Sharp
 	{
 		public IBuffer Create(EmptyFactoryCreationContext context)
 		{
-			//Copied from old buffer.create
-			Buffer stream_1 = new Buffer();
-			stream_1.position = 0;
-			stream_1.buffer = new byte[5000];
-			return stream_1;
+			//This is how the old Buffer.Create functioned, except it would try to pull from a cache. Which was dumb.
+			return new Buffer(new byte[5000]);
 		}
 	}
 }
