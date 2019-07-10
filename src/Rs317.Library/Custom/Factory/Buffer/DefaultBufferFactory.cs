0206a332ddd5a27467ad0bd143863f0b718b1323
HelloKitty <andrew.blakely@ymail.com>
2019-07-10 13:01:35 -0500

    Created DefaultBufferFactory based on buffer.create

---
+++ src/Rs317.Library/Custom/Factory/Buffer/DefaultBufferFactory.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public sealed class DefaultBufferFactory : IBufferFactory
+	{
+		public IBuffer Create(EmptyFactoryCreationContext context)
+		{
+			//Copied from old buffer.create
+			Buffer stream_1 = new Buffer();
+			stream_1.position = 0;
+			stream_1.buffer = new byte[5000];
+			return stream_1;
+		}
+	}
+}
