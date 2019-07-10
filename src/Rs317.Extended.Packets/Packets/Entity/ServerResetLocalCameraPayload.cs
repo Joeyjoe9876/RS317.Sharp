475468fee73c0ff2834a3254f7cd5de1502445bc
HelloKitty <andrew.blakely@ymail.com>
2019-07-09 19:23:17 -0500

    Fixed camera reset opcode

--- src/Rs317.Extended.Packets/Packets/Entity/ServerResetLocalCameraPayload.cs
+++ src/Rs317.Extended.Packets/Packets/Entity/ServerResetLocalCameraPayload.cs
@@ -6,7 +6,7 @@ using FreecraftCore.Serializer;
 namespace Rs317.Extended
 {
 	[WireDataContract]
-	[GameServerPayload(RsNetworkOperationCode.SetChatModeStatus)]
+	[GameServerPayload(RsNetworkOperationCode.CameraReset)]
 	public sealed class ServerResetLocalCameraPayload : BaseGameServerPayload
 	{
 		//No data in this packet, just a command.
