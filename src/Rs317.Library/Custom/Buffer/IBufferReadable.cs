8ebb80da92b90c4e7859bfab3149737d4bfc833a
HelloKitty <andrew.blakely@ymail.com>
2019-07-10 12:03:38 -0500

    Added IBufferWriteable and IBufferReadable

---
+++ src/Rs317.Library/Custom/Buffer/IBufferReadable.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public interface IBufferReadable
+	{
+		void finishBitAccess();
+
+		void generateKeys();
+
+		byte get();
+
+		int get3Bytes();
+
+		byte getByteC();
+
+		void getBytes(int startPos, int endPos, byte[] buf);
+
+		byte getByteS();
+
+		int getSignedLEShort();
+
+		int getSignedLEShortA();
+
+		int getInt();
+
+		int getMEBInt();
+
+		int getMESInt();
+
+		long getLong();
+
+		int getShort();
+
+		int getSmartA();
+
+		int getSmartB();
+
+		String getString();
+
+		int getUnsignedByte();
+
+		int getUnsignedByteA();
+
+		int getUnsignedByteC();
+
+		int getUnsignedByteS();
+
+		int getUnsignedLEShort();
+
+		int getUnsignedLEShortA();
+
+		int getUnsignedShort();
+
+		int getUnsignedShortA();
+
+		void initBitAccess();
+
+		int readBits(int i);
+
+		byte[] readBytes();
+
+		void readBytes(int length, int startPosition, byte[] dest);
+	}
+}
---
+++ src/Rs317.Library/Custom/Buffer/IBufferWriteable.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public interface IBufferWriteable
+	{
+		void put(int i);
+
+		void put24BitInt(int i);
+
+		void putByteC(int i);
+
+		void putBytes(byte[] buf, int length, int startPosition);
+
+		void putByteS(int j);
+
+		void putBytesA(int i, byte[] buf, int j);
+
+		void putInt(int i);
+
+		void putLEInt(int j);
+
+		void putLEShort(int i);
+
+		void putLEShortA(int j);
+
+		void putLong(long l);
+
+		void putOpcode(int i);
+
+		void putShort(int i);
+
+		void putShortA(int j);
+
+		void putSizeByte(int i);
+
+		void putString(String s);
+	}
+}
