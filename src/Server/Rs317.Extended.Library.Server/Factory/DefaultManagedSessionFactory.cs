9fff267a1c28bc4cec2af652612846b9c86b20ec
HelloKitty <andrew.blakely@ymail.com>
2019-07-11 06:01:14 -0500

    Added initial GladMMO implementations of session/client factories

---
+++ src/Server/Rs317.Extended.Library.Server/Factory/DefaultManagedSessionFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using GladNet;
+using JetBrains.Annotations;
+
+namespace Rs317.Extended
+{
+	public sealed class DefaultManagedSessionFactory : IManagedSessionFactory
+	{
+		private INetworkSerializationService Serializer { get; }
+
+		private ILog Logger { get; }
+
+		/// <inheritdoc />
+		public DefaultManagedSessionFactory([NotNull] INetworkSerializationService serializer, [NotNull] ILog logger)
+		{
+			Serializer = serializer ?? throw new ArgumentNullException(nameof(serializer));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		/// <inheritdoc />
+		public IManagedNetworkServerClient<BaseGameServerPayload, BaseGameClientPayload> Create(ManagedSessionCreationContext context)
+		{
+			return null;
+			//return new GladMMOUnmanagedNetworkClient<DotNetTcpClientNetworkClient, GameClientPacketPayload, GameServerPacketPayload, IGamePacketPayload>(new DotNetTcpClientNetworkClient(context.Client), Serializer, Logger)
+			//	.AsManagedSession(Logger);
+		}
+	}
+}
