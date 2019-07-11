fc1353d46a645a971ed074c07a26881929b42859
HelloKitty <andrew.blakely@ymail.com>
2019-07-11 06:02:39 -0500

    Added autofac module for session factory stuff

---
+++ src/Server/Rs317.Extended.Library.Server/Modules/SessionAutofacModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+
+namespace Rs317.Extended
+{
+	public sealed class SessionAutofacModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<DefaultManagedSessionFactory>()
+				.As<IManagedSessionFactory>();
+
+			builder.RegisterType<DefaultManagedClientSessionFactory>()
+				.As<IManagedClientSessionFactory>();
+		}
+	}
+}
