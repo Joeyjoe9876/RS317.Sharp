fae2b8eea9dba4b1d71efb021dec29f083d222a0
HelloKitty <andrew.blakely@ymail.com>
2019-07-11 11:00:28 -0500

    Added PayloadHandlerAutofacModule

---
+++ src/Server/Rs317.Extended.Library.Server/Modules/PayloadHandlerAutofacModule.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Common.Logging;
+using GladNet;
+
+namespace Rs317.Extended
+{
+	public sealed class PayloadHandlerAutofacModule : Module
+	{
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<GameServerDefaultRequestHandler>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+
+			builder.RegisterType<MessageHandlerService<BaseGameClientPayload, BaseGameServerPayload, IPeerSessionMessageContext<BaseGameServerPayload>>>()
+				.As<MessageHandlerService<BaseGameClientPayload, BaseGameServerPayload, IPeerSessionMessageContext<BaseGameServerPayload>>>()
+				.SingleInstance();
+		}
+	}
+}
