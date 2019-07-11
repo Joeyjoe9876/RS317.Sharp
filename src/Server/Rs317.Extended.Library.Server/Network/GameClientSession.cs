11fee1df5c501a2b85b434b6783969a0a0decea9
HelloKitty <andrew.blakely@ymail.com>
2019-07-11 06:02:56 -0500

    Added GameClientSession

---
+++ src/Server/Rs317.Extended.Library.Server/Network/GameClientSession.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+using JetBrains.Annotations;
+
+namespace Rs317.Extended
+{
+	/// <summary>
+	/// client session.
+	/// </summary>
+	public sealed class GameClientSession : ManagedClientSession<BaseGameServerPayload, BaseGameClientPayload>
+	{
+		/// <summary>
+		/// The message handlers service.
+		/// </summary>
+		private MessageHandlerService<BaseGameClientPayload, BaseGameServerPayload, IPeerSessionMessageContext<BaseGameServerPayload>> MessageHandlers { get; }
+
+		//TODO: One day this design fault will be fixed.
+		public static MockedPayloadInterceptorService MockedInterceptorService { get; } = new MockedPayloadInterceptorService();
+
+		private ILog Logger { get; }
+
+		/// <inheritdoc />
+		public GameClientSession(IManagedNetworkServerClient<BaseGameServerPayload, BaseGameClientPayload> internalManagedNetworkClient,
+			SessionDetails details,
+			MessageHandlerService<BaseGameClientPayload, BaseGameServerPayload, IPeerSessionMessageContext<BaseGameServerPayload>> messageHandlers,
+			[NotNull] ILog logger)
+			: base(internalManagedNetworkClient, details)
+		{
+			MessageHandlers = messageHandlers ?? throw new ArgumentNullException(nameof(messageHandlers));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+
+		/// <inheritdoc />
+		public override Task OnNetworkMessageRecieved(NetworkIncomingMessage<BaseGameClientPayload> message)
+		{
+			return MessageHandlers.TryHandleMessage(new DefaultSessionMessageContext<BaseGameServerPayload>(this.Connection, this.SendService, MockedInterceptorService, Details), message);
+		}
+	}
+}
