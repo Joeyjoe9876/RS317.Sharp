20cb7da4d5156bad44973d7cf09ab791faac06e8
HelloKitty <andrew.blakely@ymail.com>
2019-07-11 06:02:14 -0500

    Added MockedPayloadInterceptorService (again)

---
+++ src/Server/Rs317.Extended.Library.Server/Network/MockedPayloadInterceptorService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using GladNet;
+
+namespace Rs317.Extended
+{
+	public sealed class MockedPayloadInterceptorService : IPeerRequestSendService<BaseGameServerPayload>
+	{
+		/// <inheritdoc />
+		public Task<TResponseType> SendRequestAsync<TResponseType>(BaseGameServerPayload request, DeliveryMethod method = DeliveryMethod.ReliableOrdered, CancellationToken cancellationToken = new CancellationToken())
+		{
+			throw new NotSupportedException($"Servers do not support {nameof(IPeerRequestSendService<BaseGameServerPayload>)}");
+		}
+	}
+}
