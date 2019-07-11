4e9af3e9eabdfe64599846f2a07f62d5e9f55ba7
HelloKitty <andrew.blakely@ymail.com>
2019-07-11 06:00:35 -0500

    Added IManagedSessionFactory

---
+++ src/Server/Rs317.Extended.Library.Server/Factory/IManagedSessionFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GladNet;
+using Rs317.Sharp;
+
+namespace Rs317.Extended
+{
+	/// <summary>
+	/// Simplified type interface for the <see cref="IFactoryCreateable{TCreateType,TContextType}"/>
+	/// for managed sessions.
+	/// </summary>
+	public interface IManagedSessionFactory : IFactoryCreateable<IManagedNetworkServerClient<BaseGameServerPayload, BaseGameClientPayload>, ManagedSessionCreationContext>
+	{
+
+	}
+}
