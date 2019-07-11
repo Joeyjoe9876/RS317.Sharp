58d4df2599178370811d25550c09d2c37284e491
HelloKitty <andrew.blakely@ymail.com>
2019-07-11 06:02:49 -0500

    Added serialization autofac module

---
+++ src/Server/Rs317.Extended.Library.Server/Modules/SerializationModule.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using FreecraftCore.Serializer;
+using GladNet;
+using JetBrains.Annotations;
+
+namespace Rs317.Extended
+{
+	public sealed class SerializationModule : Module
+	{
+		protected override void Load([NotNull] ContainerBuilder builder)
+		{
+			if(builder == null) throw new ArgumentNullException(nameof(builder));
+
+			builder.RegisterType<SerializerService>()
+				.As<ISerializerService>()
+				.OnActivated(args =>
+				{
+					RegisterPayloads(args.Instance);
+				})
+				.SingleInstance();
+
+			builder.RegisterType<FreecraftCoreGladNetSerializerAdapter>()
+				.As<INetworkSerializationService>();
+		}
+
+		private void RegisterPayloads([NotNull] SerializerService serializer)
+		{
+			if(serializer == null) throw new ArgumentNullException(nameof(serializer));
+
+			foreach (var payload in GameServerMetadataMarker.PayloadTypes)
+				serializer.RegisterType(payload);
+		}
+	}
+}
