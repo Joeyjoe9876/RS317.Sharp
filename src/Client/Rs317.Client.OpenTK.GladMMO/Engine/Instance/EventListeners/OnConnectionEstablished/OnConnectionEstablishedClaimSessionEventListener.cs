062600cbee6c1b05143ea45197de6a51a43dbc4a
HelloKitty <andrew.blakely@ymail.com>
2019-07-15 21:36:57 -0500

    Added actual character session handling on connection

--- src/Client/Rs317.Client.OpenTK.GladMMO/Engine/Instance/EventListeners/OnConnectionEstablished/OnConnectionEstablishedClaimSessionEventListener.cs
+++ src/Client/Rs317.Client.OpenTK.GladMMO/Engine/Instance/EventListeners/OnConnectionEstablished/OnConnectionEstablishedClaimSessionEventListener.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using Glader.Essentials;
 using GladNet;
 
@@ -15,27 +17,39 @@ namespace GladMMO
 
 		private ICharacterDataRepository CharacterDataRepository { get; }
 
+		private ICharacterService CharacterService { get; }
+
 		public OnConnectionEstablishedClaimSessionEventListener(INetworkConnectionEstablishedEventSubscribable subscriptionService,
 			[NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService,
 			[NotNull] IReadonlyAuthTokenRepository authTokenRepository,
-			[NotNull] ICharacterDataRepository characterDataRepository) 
+			[NotNull] ICharacterDataRepository characterDataRepository,
+			[NotNull] ICharacterService characterService) 
 			: base(subscriptionService)
 		{
 			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
 			AuthTokenRepository = authTokenRepository ?? throw new ArgumentNullException(nameof(authTokenRepository));
 			CharacterDataRepository = characterDataRepository ?? throw new ArgumentNullException(nameof(characterDataRepository));
+			CharacterService = characterService ?? throw new ArgumentNullException(nameof(characterService));
 		}
 
 		protected override void OnEventFired(object source, EventArgs args)
 		{
 			//Once connection to the instance server is established
 			//we must attempt to claim out session on to actually fully enter.
+			Task.Factory.StartNew(async () =>
+			{
+				CharacterListResponse listResponse = await CharacterService.GetCharacters()
+					.ConfigureAwait(false);
 
-			//TODO: This is temporary
-			CharacterDataRepository.UpdateCharacterId(4);
+				await CharacterService.TryEnterSession(listResponse.CharacterIds.First())
+					.ConfigureAwait(false);
 
-			//TODO: When it comes to community servers, we should not expose the sensitive JWT to them. We need a better way to deal with auth against untrusted instance servers
-			SendService.SendMessage(new ClientSessionClaimRequestPayload(AuthTokenRepository.RetrieveWithType(), 4));
+				CharacterDataRepository.UpdateCharacterId(listResponse.CharacterIds.First());
+
+				//TODO: When it comes to community servers, we should not expose the sensitive JWT to them. We need a better way to deal with auth against untrusted instance servers
+				await SendService.SendMessage(new ClientSessionClaimRequestPayload(AuthTokenRepository.RetrieveWithType(), listResponse.CharacterIds.First()))
+					.ConfigureAwait(false);
+			});
 		}
 	}
 }
