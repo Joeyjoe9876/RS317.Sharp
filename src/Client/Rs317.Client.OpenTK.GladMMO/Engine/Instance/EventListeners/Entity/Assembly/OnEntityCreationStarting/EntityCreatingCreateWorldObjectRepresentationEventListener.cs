2ee9b412ea3753c8b2792a17773bcebc72d2b33f
HelloKitty <andrew.blakely@ymail.com>
2019-07-15 21:37:13 -0500

    Added initial/hacky player spawn on entity world creation

--- src/Client/Rs317.Client.OpenTK.GladMMO/Engine/Instance/EventListeners/Entity/Assembly/OnEntityCreationStarting/EntityCreatingCreateWorldObjectRepresentationEventListener.cs
+++ src/Client/Rs317.Client.OpenTK.GladMMO/Engine/Instance/EventListeners/Entity/Assembly/OnEntityCreationStarting/EntityCreatingCreateWorldObjectRepresentationEventListener.cs
@@ -26,27 +26,60 @@ namespace GladMMO
 	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
 	public class EntityCreatingCreateWorldObjectRepresentationEventListener : BaseSingleEventListenerInitializable<IEntityCreationStartingEventSubscribable, EntityCreationStartingEventArgs>, IEntityWorldObjectCreatedEventSubscribable
 	{
-		private IReadonlyEntityGuidMappable<IMovementData> MovementDataMappable { get; }
-
 		public event EventHandler<EntityWorldObjectCreatedEventArgs> OnEntityWorldObjectCreated;
 
 		private GladMMOOpenTkClient Client { get; }
 
+		private ICharacterDataRepository CharacterDataRepository { get; }
+
+		private IReadonlyEntityGuidMappable<IMovementData> MovementDataMappable { get; }
+
 		public EntityCreatingCreateWorldObjectRepresentationEventListener(IEntityCreationStartingEventSubscribable subscriptionService,
-			[NotNull] IReadonlyEntityGuidMappable<IMovementData> movementDataMappable,
-			[NotNull] GladMMOOpenTkClient client)
+			[NotNull] GladMMOOpenTkClient client,
+			[JetBrains.Annotations.NotNull] ICharacterDataRepository characterDataRepository,
+			[JetBrains.Annotations.NotNull] IReadonlyEntityGuidMappable<IMovementData> movementDataMappable)
 			: base(subscriptionService)
 		{
-			MovementDataMappable = movementDataMappable ?? throw new ArgumentNullException(nameof(movementDataMappable));
 			Client = client ?? throw new ArgumentNullException(nameof(client));
+			CharacterDataRepository = characterDataRepository ?? throw new ArgumentNullException(nameof(characterDataRepository));
+			MovementDataMappable = movementDataMappable ?? throw new ArgumentNullException(nameof(movementDataMappable));
 		}
 
 		protected override void OnEventFired(object source, EntityCreationStartingEventArgs args)
 		{
 			if (args.EntityGuid.EntityType == EntityType.Player)
 			{
-				//TODO: Support move than just the local player.
-				OnEntityWorldObjectCreated?.Invoke(this, new EntityWorldObjectCreatedEventArgs(args.EntityGuid, GladMMOOpenTkClient.localPlayer));
+				//TODO: move into a factory.
+				if(CharacterDataRepository.CharacterId == args.EntityGuid.EntityId)
+					//TODO: Support move than just the local player.
+					OnEntityWorldObjectCreated?.Invoke(this, new EntityWorldObjectCreatedEventArgs(args.EntityGuid, GladMMOOpenTkClient.localPlayer));
+				else
+				{
+					Client.players[args.EntityGuid.EntityId] = new Player();
+
+					Client.players[args.EntityGuid.EntityId].visible = true;
+					Client.players[args.EntityGuid.EntityId].lastUpdateTick = GladMMOOpenTkClient.tick;
+
+					Client.players[args.EntityGuid.EntityId].standAnimationId = 0x328;
+					Client.players[args.EntityGuid.EntityId].standTurnAnimationId = 0x337;
+					Client.players[args.EntityGuid.EntityId].walkAnimationId = 0x333;
+					Client.players[args.EntityGuid.EntityId].turnAboutAnimationId = 0x334;
+					Client.players[args.EntityGuid.EntityId].turnLeftAnimationId = 0x335;
+					Client.players[args.EntityGuid.EntityId].turnRightAnimationId = 0x336;
+					Client.players[args.EntityGuid.EntityId].runAnimationId = 0x338;
+
+					Client.players[args.EntityGuid.EntityId].appearanceOffset = 336413342762192;
+					Client.players[args.EntityGuid.EntityId].appearance = new int[12] { 0, 0, 0, 0, 275, 0, 285, 295, 259, 291, 300, 266 };
+					Client.players[args.EntityGuid.EntityId].name = "Unknown";
+
+					IMovementData movementData = MovementDataMappable.RetrieveEntity(args.EntityGuid);
+					Client.players[args.EntityGuid.EntityId].setPos((int)movementData.InitialPosition.x - Client.baseX, (int)movementData.InitialPosition.z - Client.baseY);
+
+					Client.localPlayers[Client.localPlayerCount++] = args.EntityGuid.EntityId;
+					Client.playersObserved[Client.playersObservedCount++] = args.EntityGuid.EntityId;
+
+					OnEntityWorldObjectCreated?.Invoke(this, new EntityWorldObjectCreatedEventArgs(args.EntityGuid, Client.players[args.EntityGuid.EntityId]));
+				}
 			}
 			else
 				OnEntityWorldObjectCreated?.Invoke(this, new EntityWorldObjectCreatedEventArgs(args.EntityGuid, new WorldObjectStub()));
