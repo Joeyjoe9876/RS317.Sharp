7ae999a31c3b5862a63def953f0ceee36d3e08ee
HelloKitty <andrew.blakely@ymail.com>
2019-11-30 23:03:27 -0600

    Fixed RsOpenTkGraphicsMode name

--- src/Client/Rs317.Client.OpenTK/Rendering/RsOpenTkGraphicsMode.cs
+++ src/Client/Rs317.Client.OpenTK/Rendering/RsOpenTkGraphicsMode.cs
@@ -5,9 +5,9 @@ using OpenTK.Graphics;
 
 namespace Rs317.Sharp
 {
-	public sealed class RsOpenTkGraphicsMocde : GraphicsMode
+	public sealed class RsOpenTkGraphicsMode : GraphicsMode
 	{
-		public RsOpenTkGraphicsMocde()
+		public RsOpenTkGraphicsMode()
 			: base(GraphicsMode.Default.ColorFormat, 24, 8, 0) //Last parameter is FXAA setting. But it doesn't seem to affect anything right now. Must not be screenspace?
 		{
 			
