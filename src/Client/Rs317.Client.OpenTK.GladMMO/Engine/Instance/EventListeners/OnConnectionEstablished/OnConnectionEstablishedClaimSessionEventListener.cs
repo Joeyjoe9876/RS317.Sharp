3da9cef21a5b2e6f84d159d5e6f107247e6c7593
HelloKitty <andrew.blakely@ymail.com>
2020-01-04 18:22:27 -0600

    Added logging to OnConnectionEstablishedClaimSessionEventListener

--- src/Client/Rs317.Client.OpenTK.GladMMO/Engine/Instance/EventListeners/OnConnectionEstablished/OnConnectionEstablishedClaimSessionEventListener.cs
+++ src/Client/Rs317.Client.OpenTK.GladMMO/Engine/Instance/EventListeners/OnConnectionEstablished/OnConnectionEstablishedClaimSessionEventListener.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Logging;
 using Glader.Essentials;
 using GladNet;
 
@@ -19,17 +20,21 @@ namespace GladMMO
 
 		private ICharacterService CharacterService { get; }
 
+		private ILog Logger { get; }
+
 		public OnConnectionEstablishedClaimSessionEventListener(INetworkConnectionEstablishedEventSubscribable subscriptionService,
 			[NotNull] IPeerPayloadSendService<GameClientPacketPayload> sendService,
 			[NotNull] IReadonlyAuthTokenRepository authTokenRepository,
 			[NotNull] ILocalCharacterDataRepository characterDataRepository,
-			[NotNull] ICharacterService characterService) 
+			[NotNull] ICharacterService characterService,
+			[JetBrains.Annotations.NotNull] ILog logger) 
 			: base(subscriptionService)
 		{
 			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
 			AuthTokenRepository = authTokenRepository ?? throw new ArgumentNullException(nameof(authTokenRepository));
 			CharacterDataRepository = characterDataRepository ?? throw new ArgumentNullException(nameof(characterDataRepository));
 			CharacterService = characterService ?? throw new ArgumentNullException(nameof(characterService));
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
 		}
 
 		protected override void OnEventFired(object source, EventArgs args)
@@ -38,17 +43,27 @@ namespace GladMMO
 			//we must attempt to claim out session on to actually fully enter.
 			Task.Factory.StartNew(async () =>
 			{
-				CharacterListResponse listResponse = await CharacterService.GetCharacters()
-					.ConfigureAwait(false);
+				try
+				{
+					CharacterListResponse listResponse = await CharacterService.GetCharacters()
+						.ConfigureAwait(false);
 
-				await CharacterService.TryEnterSession(listResponse.CharacterIds.First())
-					.ConfigureAwait(false);
+					await CharacterService.TryEnterSession(listResponse.CharacterIds.First())
+						.ConfigureAwait(false);
 
-				CharacterDataRepository.UpdateCharacterId(listResponse.CharacterIds.First());
+					CharacterDataRepository.UpdateCharacterId(listResponse.CharacterIds.First());
 
-				//TODO: When it comes to community servers, we should not expose the sensitive JWT to them. We need a better way to deal with auth against untrusted instance servers
-				await SendService.SendMessage(new ClientSessionClaimRequestPayload(AuthTokenRepository.RetrieveWithType(), listResponse.CharacterIds.First()))
-					.ConfigureAwait(false);
+					//TODO: When it comes to community servers, we should not expose the sensitive JWT to them. We need a better way to deal with auth against untrusted instance servers
+					await SendService.SendMessage(new ClientSessionClaimRequestPayload(AuthTokenRepository.RetrieveWithType(), listResponse.CharacterIds.First()))
+						.ConfigureAwait(false);
+				}
+				catch (Exception e)
+				{
+					if(Logger.IsErrorEnabled)
+						Logger.Error($"Failed to authenticate to instance server as character. Reason: {e.ToString()}");
+
+					throw;
+				}
 			});
 		}
 	}
