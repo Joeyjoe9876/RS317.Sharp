4a6d5e004e9cdd0161b6bca515d1a97c048b3459
HelloKitty <andrew.blakely@ymail.com>
2020-01-04 18:22:34 -0600

    Added StubGeneralErrorEventPublisher

---
+++ src/Client/Rs317.Client.OpenTK.GladMMO/Engine/Instance/EventPublishers/StubGeneralErrorEventPublisher.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Glader.Essentials;
+using GladMMO;
+
+namespace Rs317.GladMMO
+{
+	[AdditionalRegisterationAs(typeof(IGeneralErrorEncounteredEventPublisher))]
+	[SceneTypeCreateGladMMO(GameSceneType.InstanceServerScene)]
+	public sealed class StubGeneralErrorEventPublisher : IGeneralErrorEncounteredEventPublisher, IGameInitializable
+	{
+		public void PublishEvent(object sender, GeneralErrorEncounteredEventArgs eventArgs)
+		{
+			Console.WriteLine($"Error: {eventArgs.ErrorTitle} Message: {eventArgs.ErrorMessage}");
+		}
+
+		public Task OnGameInitialized()
+		{
+			return Task.CompletedTask;
+		}
+	}
+}
