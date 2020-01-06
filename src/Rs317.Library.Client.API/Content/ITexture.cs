5db82044674298f363ff77c6d7320e670bfd1a07
HelloKitty <andrew.blakely@ymail.com>
2020-01-06 17:12:58 -0600

    Added ITexture to the API

---
+++ src/Rs317.Library.Client.API/Content/ITexture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Rs317.Sharp
+{
+	//Based on: https://raw.githubusercontent.com/runelite/runelite/master/runelite-api/src/main/java/net/runelite/api/Texture.java
+	public interface ITexture
+	{
+		int[] Pixels { get; }
+
+		int AnimationDirection { get; }
+
+		int AnimationSpeed { get; }
+
+		bool isLoaded { get; }
+
+		float UVCoordU { get; set; }
+
+		float UVCoordV { get; set; }
+	}
+}
