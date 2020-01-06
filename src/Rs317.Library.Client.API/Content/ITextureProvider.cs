6e996c503202e0c9a49688e478aa7ba3966094a7
HelloKitty <andrew.blakely@ymail.com>
2020-01-06 17:13:06 -0600

    Added ITextureProvider to the API

---
+++ src/Rs317.Library.Client.API/Content/ITextureProvider.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public interface ITextureProvider
+	{
+		/**
+		 * Set the brightness for textures, clearing the texture cache.
+		 *
+		 * .9 is the darkest value available in the standard options
+		 * .6 is the brightest value
+		 */
+		double Brightness { get; set; }
+
+		/**
+		 * Get all textures
+		 */
+		ITexture[] Textures { get; }
+
+		/**
+		 * Get the pixels for a texture
+		 */
+		int[] GetTexturePixels(int textureId);
+	}
+}
