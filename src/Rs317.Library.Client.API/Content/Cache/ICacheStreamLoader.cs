dd05fb36d185b6ebc723d637503dc2c16dad9306
HelloKitty <andrew.blakely@ymail.com>
2020-01-10 19:03:26 -0600

    Added ICacheStreamLoader

---
+++ src/Rs317.Library.Client.API/Content/Cache/ICacheStreamLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	//TODO: make async
+	public interface ICacheStreamLoader
+	{
+		/// <summary>
+		/// Loads the Cache.dat file as a stream.
+		/// </summary>
+		/// <returns>A stream containing the cache.dat file.</returns>
+		Stream LoadCacheDatFile();
+
+		/// <summary>
+		/// Loads the idx cache file with the provided <see cref="index"/>.
+		/// </summary>
+		/// <param name="index"></param>
+		/// <returns>Stream containing the index file.</returns>
+		Stream LoadCacheIndexFile(int index);
+	}
+}
