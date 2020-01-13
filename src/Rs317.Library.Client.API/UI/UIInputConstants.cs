80dd56687668b41f16e6e450fab636570297b956
HelloKitty <andrew.blakely@ymail.com>
2020-01-12 18:15:21 -0600

    Added UIInputConstants

---
+++ src/Rs317.Library.Client.API/UI/UIInputConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public static class UIInputConstants
+	{
+		public const int MAX_USERNAME_INPUT_LIMIT = 12;
+
+		public const int MAX_PASSWORD_INPUT_LIMIT = 20;
+	}
+}
