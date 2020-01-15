bc01691d474190dc80f3499338b3c0ccf8dbd107
HelloKitty <andrew.blakely@ymail.com>
2020-01-14 23:52:23 -0600

    Removed redundant entity spawn code

--- src/Client/Rs317.Client.Unity.GladMMO/Modules/GladMMOClientExplicitEngineInterfaceAutoModule.cs
+++ src/Client/Rs317.Client.Unity.GladMMO/Modules/GladMMOClientExplicitEngineInterfaceAutoModule.cs
@@ -4,6 +4,7 @@ using System.Text;
 using Autofac;
 using Glader.Essentials;
 using GladMMO;
+using GladNet;
 
 namespace Rs317.GladMMO
 {
@@ -25,6 +26,25 @@ namespace Rs317.GladMMO
 			builder.RegisterType<OnStartRestartNetworkClientHandlingInititablize>()
 				.As<INetworkConnectionEstablishedEventSubscribable>()
 				.As<IGameStartable>()
+				.SingleInstance();
+
+			builder.RegisterType<ClientEntityDespawnTickable>()
+				.As<IGameTickable>()
+				.As<IEntityDeconstructionStartingEventSubscribable>()
+				.As<IEntityDeconstructionFinishedEventSubscribable>()
+				.As<IGameInitializable>()
+				.SingleInstance();
+
+			builder.RegisterType<EntitySpawnTickable>()
+				.As<IEntityCreationStartingEventSubscribable>()
+				.As<IEntityCreationFinishedEventSubscribable>()
+				.As<IGameTickable>()
+				.As<IGameInitializable>()
+				.SingleInstance();
+
+			builder.RegisterType<NetworkVisibilityChangeEventHandler>()
+				.As<INetworkEntityVisibilityLostEventSubscribable>()
+				.As<IPeerMessageHandler<GameServerPacketPayload, GameClientPacketPayload>>()
 				.SingleInstance();
 		}
 	}
