cf17f637483f306116810046dfb5c91a60c7e3bf
HelloKitty <andrew.blakely@ymail.com>
2020-01-16 06:55:45 -0600

    Added RsClientConstants

---
+++ src/Rs317.Library.Client.API/RsClientConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public static class RsClientConstants
+	{
+		public const long RsClientTicksInTicks = TimeSpan.TicksPerMillisecond * RsClientMillisecondsPerTicks;
+
+		public const long RsClientMillisecondsPerTicks = 600;
+	}
+}
