3a4157ecb9c8628132a719e5e0710a5087a23799
HelloKitty <andrew.blakely@ymail.com>
2020-01-15 23:34:41 -0600

    Added ConnectionInititializationType

---
+++ src/Rs317.Library.Client.API/ConnectionInititializationType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	/// <summary>
+	/// Enumeration of the types of connection initialization
+	/// that the RS client can have.
+	/// </summary>
+	public enum ConnectionInititializationType : byte
+	{
+		/// <summary>
+		/// The connection is being initialized with
+		/// a login/auth request.
+		/// </summary>
+		LoginRequest = 14,
+
+		Update = 15,
+
+		NewConnection = 16,
+
+		ReconnectionL = 18
+	}
+}
