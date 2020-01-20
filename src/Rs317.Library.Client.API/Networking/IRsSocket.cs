06557341595e4ea7b53eac284b4e71bd37bcd750
HelloKitty <andrew.blakely@ymail.com>
2020-01-19 22:37:28 -0600

    Created IRsSocket interfaces

---
+++ src/Rs317.Library.Client.API/Networking/IRsSocket.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public interface IRsSocket
+	{
+		void write(int i, byte[] abyte0);
+
+		void read(byte[] abyte0, int j);
+
+		int read();
+
+		int available();
+
+		void close();
+	}
+}
---
+++ src/Rs317.Library.Client.API/Networking/SocketCreationContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public sealed class SocketCreationContext
+	{
+		public string Endpoint { get; }
+
+		public int Port { get; }
+
+		public SocketCreationContext(string endpoint, int port)
+		{
+			if (port <= 0) throw new ArgumentOutOfRangeException(nameof(port));
+
+			Endpoint = endpoint ?? throw new ArgumentNullException(nameof(endpoint));
+			Port = port;
+		}
+	}
+}
