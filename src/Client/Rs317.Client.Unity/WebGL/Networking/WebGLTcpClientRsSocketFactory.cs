3107f8b44e1b398ab209c47e8967dbcf8f6d38a8
HelloKitty <andrew.blakely@ymail.com>
2020-01-20 02:09:04 -0600

    Added WebGLTcpClientRsSocketFactory

---
+++ src/Client/Rs317.Client.Unity/WebGL/Networking/WebGLTcpClientRsSocketFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Sockets;
+using System.Text;
+
+namespace Rs317.Sharp
+{
+	public sealed class WebGLTcpClientRsSocketFactory : IRsSocketFactory
+	{
+		public IRsSocket Create(SocketCreationContext context)
+		{
+			return new WebGLTcpClientRsSocket(new TcpClient(context.Endpoint, context.Port));
+		}
+	}
+}
