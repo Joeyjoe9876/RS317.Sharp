6c391f5dac3c72b30e0759aa815899e4dd989735
HelloKitty <andrew.blakely@ymail.com>
2020-01-19 22:37:51 -0600

    Created DefaultRunnableStarterStrategy

---
+++ src/Rs317.Library.Client/Custom/Threading/DefaultRunnableStarterStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rs317.Sharp
+{
+	public sealed class DefaultRunnableStarterStrategy : IRunnableStarter
+	{
+		public void StartRunnable(IRunnable runnable, int priority)
+		{
+			//Run it on the threadpool instead.
+			Task.Factory.StartNew(runnable.run, priority < 1 ? TaskCreationOptions.LongRunning : TaskCreationOptions.None);
+		}
+	}
+}
