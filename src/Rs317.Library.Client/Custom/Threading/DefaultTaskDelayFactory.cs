cca2118af38bd82680d315dfc423efc8f8dd9b84
HelloKitty <andrew.blakely@ymail.com>
2020-01-23 00:01:31 -0600

    Added DefaultTaskDelayFactory

---
+++ src/Rs317.Library.Client/Custom/Threading/DefaultTaskDelayFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rs317.Sharp
+{
+	public sealed class DefaultTaskDelayFactory : ITaskDelayFactory
+	{
+		public Task Create(int context)
+		{
+			return Task.Delay(context);
+		}
+	}
+}
